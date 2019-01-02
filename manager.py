4764b74e367f43bf08dbc8278bd4de2d8ad8827e
“zhoudoudou” <852014708@qq.com>
2019-01-02 19:41:29 +0800

    add code comment

--- manager.py
+++ manager.py
@@ -16,29 +16,39 @@ from multiprocessing.managers import BaseManager
 task_queue = queue.Queue(maxsize = 10)
 # 结果队列
 result_queue = queue.Queue(maxsize = 10)
+# 以上两个队列，之后会以"task"和"result"的别名，在整个局域网内共享
+# 只要与主机进行连接，便可随时对这两个列表进行操作，且列表是绝对干净的
 
 class QueueManager(BaseManager):
     pass
-
+# 将任务队列task_queue和result_queue进行注册，并分别取名为task和result
+# 并将其暴露在局域网内
 QueueManager.register('task', callable=lambda: task_queue)
 QueueManager.register('result', callable=lambda: result_queue)
-
 manager = QueueManager(address=('', 5000),authkey=b'abc')
 manager.start()
-
 task = manager.task()
 result = manager.result()
 
-task_init = [0]
+# 这里我们设计了一个非常简单的例子来进行我们的验证和理解我们的计算系统
+# 我们的任务是，初始化10个list，每个list的里面有一个0值[0]（我们将其定义为任务,共计10个)
+# 我们利用局域网的上的计算设备(本例两台),分别对这个10个list进行进行+1操作
+#
+#
+task_init = [0] #初始化单个任务
+# 将其循环塞入任务队列中，从而形成具有10个待执行任务的任务队列
+# 此时因为任务队列（task）的里面的任务尚未被执行，因此结果队列（result）为空
 for i in range(10):
     task.put(task_init)
 
-while(~result.full()):
+
+while(~result.full()):# 检测结果队列队列是否满，结果队列如果full的话，说明所有任务已经处理完毕
     time.sleep(2)
-    while (result.full()):
+    while (result.full()): # 如果结果队列满了，说明任务队列已经处理完毕，我们可以对结果队列进行一些处理，从而生成新的任务
         temp = []
         print("now the result is ",result)
-        # 将result的结果直接塞入task队列
+        # 实际中，我们可能会在结果队列的基础上，做一定的操作，再生成新的任务队列
+        # 此处为了简化，直接将结果队列，一一弹出，塞入任务队列
         for i in range(10):
             temp.append(result.get())
         print(temp)
--- worker_2.py
+++ worker_2.py
@@ -15,17 +15,19 @@ class QueueManager(BaseManager):
 
 QueueManager.register("task")
 QueueManager.register("result")
-
-server_addr = "127.0.0.1"
+# 本代码在本地机器上测试（一台电脑），因此server的地址写成"127.0.0.1"
+# 不过你的work_1.py和work_2.py的代码实在同一局域网的不同机器上，此处请写manager.py运行的机器IP地址
+server_addr = "127.0.0.1"#
 print("connecting to server %s" % server_addr)
 worker = QueueManager(address=(server_addr, 5000), authkey=b'abc')
 worker.connect()
 print("conneted succeed")
-
 task = worker.task()
 result = worker.result()
+
+
 while(1):
-    while(task.empty()):
+    while(task.empty()):# 判断任务队列中是否还有未执行的任务
         time.sleep(1)
     time.sleep(np.random.rand())
     # 从任务队列中去任务
